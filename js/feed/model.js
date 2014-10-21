43a2708c77e561039b223921320d0bbf010f7371
ofs-vinoth <vinoth.nayagam@object-frontier.com>
2014-10-21 20:10:28 +0530

    Feed Page changes

---
+++ js/feed/model.js
@@ -0,0 +1,51 @@
+/**
+ * Feed Class
+ * id - > timestamp
+ * type -> URL or TEXT
+ */ 
+function Feed(id,type) {
+	this.id = id;
+	this.type = type;
+};
+Feed.prototype = {
+	getId: function() {
+		return this.id; 
+	},
+	getType: function() {
+		return this.type;
+	}
+};
+
+/**
+ * URLFeed Class
+ * extends from Feed
+ */ 
+function URLFeed(id, url) {
+	this.id = id;
+	this.type = "URL";
+	this.url = url;
+	
+	
+};
+URLFeed.prototype = {
+	getFeed: function() {
+		return this.url; 
+	}
+};
+URLFeed.prototype = Object.create(Feed.prototype, URLFeed.prototype);
+
+/**
+ * TextFeed Class
+ * extends from Feed
+ */ 
+function TextFeed(id, text){
+	this.id = id;
+	this.type = "Text";
+	this.text = text;	
+};
+TextFeed.prototype = {
+	getFeed: function() {
+		return this.text; 
+	}
+};
+TextFeed.prototype = Object.create(Feed.prototype, TextFeed.prototype);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,53 @@
+/**
+ * LOGIN Class
+ * Do all Login page related operation like sign in, validation .. etc.
+ *
+ */
 var LOGIN = function() {
 	
 	var username = "";
 	var password = "";
 	
 	return {
+		/**
+		 * Get username & password from text field.
+		 * Validate username & password.
+		 * Once user credential validated, it will redirect to Feed page.
+		 */
 		signIn: function() {
 			username = document.getElementById("usename").value;
 			password = document.getElementById("password").value;
-			if ("vinoth" == username && "raj" == password) {
-				window.location = "home.html"
-			} else {
-				alert("Login Failed");
+			try {
+				if (!this.validate(username, password)) return;
+				
+				if ("raj" == username && "vinoth" == password) {
+					window.location = "feed.html"
+					return;
+				} else {
+										
+				}
+			} catch (err) {
+				alert(err);
 				document.getElementById("usename").value = "";
 				document.getElementById("password").value = "";
+			}			
+		},
+		
+		/**
+		 * Validate username & password.
+		 * Username field Should be 1-8 characters .
+		 * Password field should be 1-6 characters
+		 */
+		validate:function(username, password) {
+		
+			var message = null;
+			if (username == null || username.length <1 || username.length > 8) {
+				throw("Invalid User Name : User Name should not empty & gretaer than 8 characters");
+				
+			} else if (password == null || password.length < 1 || password.length < 6) {
+				throw("Invalid Password : Password should not empty & less than 6 characters");				
 			}
+			return true;
 		}
 	}
 
